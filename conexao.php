a6df8d043faea4e8d0c2390a58e48d932c984937
Mah Escame <159040587+Mahescame@users.noreply.github.com>
2024-05-10 12:08:46 -0400

    Add files via upload

---
+++ conexao.php
@@ -0,0 +1,22 @@
+<?php
+
+
+define ("SERVIDOR", "localhost");
+define ("USUARIO", "root");
+define ("SENHA", "");
+define ("BANCO", "crud_fabio");
+define ("PORTA", "3306");
+
+function abrirBanco() {
+
+    $con = new mysqli(SERVIDOR, USUARIO, SENHA, BANCO, PORTA);
+
+    if($con->connect_error) {
+        die("Falha na conexão: " . $con->connect_error);
+    }
+    return $con;
+}
+
+    function fecharBanco($con) {
+        $con->close();
+    }
